a71faed96d8516d15dfeef0222267cbafa21b21b
Olszewskim <olszewskim1991@gmail.com>
2020-03-19 17:54:15 +0100

    Building working states

--- Assets/Scripts/GameCore/Models/WarehouseData.cs
+++ Assets/Scripts/GameCore/Models/WarehouseData.cs
@@ -1,9 +1,8 @@
-﻿
-using System;
+﻿using System;
+using static Enums;
 
 [Serializable]
-public class WarehouseData
-{
+public class WarehouseData {
     public BigInteger Capacity { get; private set; }
     public double CargoPackageProducingSpeed { get; private set; }
     public BigInteger TotalCargoAmountInPackage { get; private set; }
@@ -12,6 +11,7 @@ public class WarehouseData
 
     public BigInteger CurrentCargoAmount { get; private set; }
 
+    private BuildingWorkingState _currentWorkingState;
     private double _currentProductionCycle;
 
     public WarehouseData() {
@@ -19,21 +19,41 @@ public class WarehouseData
         CargoPackageProducingSpeed = Constants.WAREHOUSE_BASE_CARGO_PACKAGE_PRODUCING_SPEED;
         TotalCargoAmountInPackage = Constants.WAREHOUSE_BASE_CARGO_AMOUNT_IN_PACKAGE;
         _currentProductionCycle = 0;
+        StartProduction();
+    }
+
+    private void StartProduction() {
+        _currentProductionCycle = 0;
+        _currentWorkingState = BuildingWorkingState.Working;
     }
 
     public void UpdateWarehouse(float deltaTime) {
-        _currentProductionCycle += deltaTime;
-        if (_currentProductionCycle >= CargoPackageProducingSpeed) {
-            FinishCargoPackageProduction();
+        if (IsWorking()) {
+            _currentProductionCycle += deltaTime;
+            if (_currentProductionCycle >= CargoPackageProducingSpeed) {
+                FinishCargoPackageProduction();
+            }
         }
+    }
 
+    private bool IsWorking() {
+        return _currentWorkingState == BuildingWorkingState.Working;
     }
 
     private void FinishCargoPackageProduction() {
         _currentProductionCycle = 0;
         CurrentCargoAmount += TotalCargoAmountInPackage;
-        if (CurrentCargoAmount > Capacity) {
+        if (IsFull()) {
             CurrentCargoAmount = Capacity;
+            StopProduction();
         }
+    }
+
+    private bool IsFull() {
+        return CurrentCargoAmount >= Capacity;
+    }
+
+    private void StopProduction() {
+        _currentWorkingState = BuildingWorkingState.Full;
     }
 }
---
+++ Assets/Scripts/Utilities/Enums.cs
@@ -0,0 +1,7 @@
+﻿public static class Enums {
+    public enum BuildingWorkingState {
+        Waiting,
+        Working,
+        Full
+    }
+}
