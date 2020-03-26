74ffc2eb6fae15c0e213292a8c4c3d7530ce78bb
Olszewskim <olszewskim1991@gmail.com>
2020-03-26 19:10:18 +0100

    Feature: Basic trolley view

---
+++ Assets/Scripts/GameCore/Views/TrolleyView.cs
@@ -0,0 +1,19 @@
+﻿using GameCore.Models;
+using IdleTransport.Managers;
+using UnityEngine;
+
+namespace GameCore.Views
+{
+    public class TrolleyView : MonoBehaviour
+    {
+        private TrolleyData _trolleyData;
+
+        private void Start() {
+            _trolleyData = PlayerManager.Instance.Player.TrolleyData;
+        }
+
+        private void Update() {
+            _trolleyData.UpdateTrolley(Time.deltaTime);
+        }
+    }
+}
