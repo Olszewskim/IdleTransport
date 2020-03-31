822f36c3751271ea632171409a127f97356473bd
Olszewskim <olszewskim1991@gmail.com>
2020-03-31 18:23:01 +0200

    Feature: elevator view

---
+++ Assets/Scripts/GameCore/Views/ElevatorView.cs
@@ -0,0 +1,10 @@
+﻿using IdleTransport.Managers;
+
+namespace IdleTransport.GameCore.Views {
+    public class ElevatorView : UnitView {
+        protected override void Init() {
+            unitData = PlayerManager.Instance.Player.ElevatorData;
+            base.Init();
+        }
+    }
+}
