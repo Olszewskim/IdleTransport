39b59836b801e2447125bd6897b832fdb35d6838
Olszewskim <olszewskim1991@gmail.com>
2020-04-02 20:02:52 +0200

    Refactoring: change ease of moving animations to linear

--- Assets/Scripts/GameCore/Views/TransportingUnitView.cs
+++ Assets/Scripts/GameCore/Views/TransportingUnitView.cs
@@ -20,12 +20,12 @@ namespace IdleTransport.GameCore.Views
         private void StartTransportingAnimation() {
             var destinationPos = new Vector3(_transportingDestinationTransform.transform.position.x, transform.position.y,
                 transform.position.z);
-            transform.DOMove(destinationPos, (float) _transportingUnitData.WalkingSpeed);
+            transform.DOMove(destinationPos, (float) _transportingUnitData.WalkingSpeed).SetEase(Ease.Linear);
             unitSpriteRenderer.flipX = false;
         }
 
         private void StartReturningAnimation() {
-            transform.DOMove(_startPos, (float) _transportingUnitData.WalkingSpeed);
+            transform.DOMove(_startPos, (float) _transportingUnitData.WalkingSpeed).SetEase(Ease.Linear);
             unitSpriteRenderer.flipX = true;
         }
 
